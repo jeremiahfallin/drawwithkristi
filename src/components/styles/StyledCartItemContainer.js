ae1e5fddba35ee9970471f97af45b6ed3cb44c16
Tacit <jeremiah.fallin@gmail.com>
2020-10-24 22:35:17 -0700

    netlify checkout

--- src/components/styles/StyledCartItemContainer.js
+++ src/components/styles/StyledCartItemContainer.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledCartItemContainer = styled.article`
   display: grid;
-  grid-template-rows: auto 100px;
+  grid-template-rows: 100% 100px;
 `;
 
 export default StyledCartItemContainer;

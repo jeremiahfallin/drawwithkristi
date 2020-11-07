9dfd780c972e4ff9048807c20e21bcc0a256a47c
Tacit <jeremiah.fallin@gmail.com>
2020-11-07 11:47:38 -0800

    changed where styles are saved.

--- src/components/styles/CartItem.styles.js
+++ src/components/styles/CartItem.styles.js
@@ -4,7 +4,6 @@ const StyledCartItemContainer = styled.article`
   display: grid;
   grid-template-rows: 50px minmax(auto, 118px);
   gap: 8px;
-  height: 100%;
 `;
 
 const StyledCartItemQuantityContainer = styled.div`

baa435c178efba71b70e413976e5487fdd47d37c
Tacit <jeremiah.fallin@gmail.com>
2020-11-08 11:17:44 -0800

    firefox!!

--- src/components/styles/CartItem.styles.js
+++ src/components/styles/CartItem.styles.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const StyledCartItemContainer = styled.article`
   display: grid;
-  grid-template-rows: repeat(auto-fill, minmax(min(10rem, 100%), 1fr));
+  /* grid-template-rows: repeat(auto-fill, minmax(min(10rem, 100%), 1fr)); */
+  grid-template-rows: auto 200px;
   gap: 8px;
 `;
 
@@ -11,5 +12,6 @@ const StyledCartItemQuantityContainer = styled.div`
   gap: 8px;
   grid-template-columns: auto 50px; */
 `;
+
 export { StyledCartItemQuantityContainer };
 export default StyledCartItemContainer;
